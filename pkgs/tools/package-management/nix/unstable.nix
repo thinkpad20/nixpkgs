1d82753cdee24ce014da79f5333b12a4261501ae
Joel Taylor <me@joelt.io>
2014-08-18 14:31:37 -0700

    stuff

--- pkgs/tools/package-management/nix/unstable.nix
+++ pkgs/tools/package-management/nix/unstable.nix
@@ -24,9 +24,6 @@ stdenv.mkDerivation rec {
   postUnpack =
     '' export CPATH="${bzip2}/include"
        export LIBRARY_PATH="${bzip2}/lib"
-       export CXXFLAGS="-O3 ${stdenv.lib.optionalString stdenv.isDarwin
-         "-mmacosx-version-min=10.7 -std=c++11 -stdlib=libc++"
-       }"
     '';
 
   configureFlags =
@@ -37,7 +34,7 @@ stdenv.mkDerivation rec {
       --with-www-curl=${perlPackages.WWWCurl}/${perl.libPrefix}
       --disable-init-state
       --enable-gc
-      CFLAGS=-O3
+      CFLAGS=-O3 CXXFLAGS=-O3
     '';
 
   makeFlags = "profiledir=$(out)/etc/profile.d";
