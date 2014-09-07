71b21b33ed2d20f2cdbc996f20c97cf4e720cf89
Joel Taylor <me@joelt.io>
2014-09-07 12:06:05 -0700

    update to clang 3.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2804,6 +2804,7 @@ let
 
   clang = wrapClang llvmPackages.clang;
 
+  clang_35 = wrapClang llvmPackages_35.clang;
   clang_34 = wrapClang llvmPackages_34.clang;
   clang_33 = wrapClang (clangUnwrapped llvm_33 ../development/compilers/llvm/3.3/clang.nix);
 
@@ -3337,13 +3338,17 @@ let
 
   llvm_v = path: callPackage path { };
 
-  llvmPackages = llvmPackages_34;
+  llvmPackages = llvmPackages_35;
 
   llvmPackages_34 = recurseIntoAttrs (import ../development/compilers/llvm/3.4 {
     inherit stdenv newScope fetchurl;
     isl = isl_0_12;
   });
   llvmPackagesSelf = import ../development/compilers/llvm/3.4 { inherit newScope fetchurl; isl = isl_0_12; stdenv = libcxxStdenv; };
+
+  llvmPackages_35 = import ../development/compilers/llvm/3.5 {
+    inherit stdenv newScope fetchurl isl;
+  };
 
   manticore = callPackage ../development/compilers/manticore { };
 
@@ -11932,7 +11937,6 @@ let
   };
 
   nixUnstable = callPackage ../tools/package-management/nix/unstable.nix {
-    stdenv = if stdenv.isDarwin then allStdenvs.stdenvDarwin33 else stdenv;
     storeDir = config.nix.storeDir or "/nix/store";
     stateDir = config.nix.stateDir or "/nix/var";
   };
