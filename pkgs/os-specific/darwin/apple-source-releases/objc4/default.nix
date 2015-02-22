1269086df4f9e25be2701235f27397f1cabffbe5
Daniel Peebles <pumpkin@me.com>
2015-02-22 03:41:05 -0500

    Slightly more precise libobjc (allowing relinking)

--- pkgs/os-specific/darwin/apple-source-releases/objc4/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/objc4/default.nix
@@ -14,7 +14,9 @@ appleDerivation {
 
   installPhase = ''
     mkdir -p $out/include/objc $out/lib
-    ln -s /usr/lib/libobjc.dylib $out/lib/libobjc.dylib
+    ln -s /usr/lib/libobjc.A.dylib $out/lib/libobjc.A.dylib
+    ln -s libobjc.A.dylib $out/lib/libobjc.dylib
+
     cp runtime/OldClasses.subproj/List.h $out/include/objc/List.h
     cp runtime/NSObjCRuntime.h $out/include/objc/NSObjCRuntime.h
     cp runtime/NSObject.h $out/include/objc/NSObject.h
