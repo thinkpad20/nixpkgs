d5ecb5a80961f3cc530dce79cbeddd85dc4d88e2
Daniel Peebles <pumpkin@me.com>
2015-02-22 17:32:25 -0500

    Whoops, libresolv needs a version

--- pkgs/os-specific/darwin/apple-source-releases/libresolv/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libresolv/default.nix
@@ -33,7 +33,9 @@ appleDerivation {
     cc -I. -c res_send.c
     cc -I. -c res_sendsigned.c
     cc -I. -c res_update.c
-    cc -dynamiclib -install_name $out/lib/libresolv.9.dylib -o libresolv.9.dylib *.o
+    cc -dynamiclib -install_name $out/lib/libresolv.9.dylib \
+      -compatibility_version 1.0 -current_version 1.0 \
+      -o libresolv.9.dylib *.o
   '';
 
   installPhase = ''
