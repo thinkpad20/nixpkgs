8c71b0908ec736d3d85ef3bfcd71d73a027ff4e9
Allen Nelson <anelson@narrativescience.com>
2016-04-30 22:05:04 +0000

    stop lsof from using impure paths

--- pkgs/development/tools/misc/lsof/default.nix
+++ pkgs/development/tools/misc/lsof/default.nix
@@ -15,14 +15,16 @@ stdenv.mkDerivation rec {
     sha256 = "061p18v0mhzq517791xkjs8a5dfynq1418a1mwxpji69zp2jzb41";
   };
 
+  LSOF_INCLUDE = "/dev/null";
+
   unpackPhase = "tar xvjf $src; cd lsof_*; tar xvf lsof_*.tar; sourceRoot=$( echo lsof_*/); ";
-  
+
   preBuild = "sed -i Makefile -e 's/^CFGF=/&	-DHASIPv6=1/;';";
-  
+
   configurePhase = if stdenv.isDarwin
     then "./Configure -n darwin;"
     else "./Configure -n linux;";
-  
+
   installPhase = ''
     mkdir -p $out/bin $out/man/man8
     cp lsof.8 $out/man/man8/
