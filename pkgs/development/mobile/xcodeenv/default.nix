2416719d9703a8943e21009d011db139092aa661
Allen Nelson <anelson@narrativescience.com>
2016-05-02 15:30:40 -0700

    modify xcodeenv version

--- pkgs/development/mobile/xcodeenv/default.nix
+++ pkgs/development/mobile/xcodeenv/default.nix
@@ -1,4 +1,4 @@
-{stdenv, version ? "7.2", xcodeBaseDir ? "/Applications/Xcode.app"}:
+{stdenv, version ? "6.4", xcodeBaseDir ? "/Applications/Xcode.app"}:
 
 rec {
   xcodewrapper = import ./xcodewrapper.nix {
