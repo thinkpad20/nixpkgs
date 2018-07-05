68bb6223d850d10fc5a6c6dc82abf72bd1810621
Allen Nelson <ithinkican@gmail.com>
2018-07-05 17:25:11 -0500

    get patched pspp building
    add patch to make outfile command optional
    
    get required info into the MDD generator

--- pkgs/applications/science/math/pspp/default.nix
+++ pkgs/applications/science/math/pspp/default.nix
@@ -1,8 +1,21 @@
 { stdenv, fetchurl, libxml2, readline, zlib, perl, cairo, gtk3, gsl
-, pkgconfig, gtksourceview, pango, gettext
+, pkgconfig, gtksourceview, pango, gettext, automake, autoconf
 , makeWrapper, gsettings-desktop-schemas, hicolor-icon-theme
-, gnome3
+, gnome3, texinfo
+, headless ? stdenv.isDarwin
 }:
+
+let
+  # It requires this specific version of automake since it's redoing
+  # automake commands on top of files which already exist.
+  automake_1_15 = automake.overrideDerivation (d: rec {
+    name = "automake-1.15";
+    src = fetchurl {
+      url = "mirror://gnu/automake/${name}.tar.xz";
+      sha256 = "0dl6vfi2lzz8alnklwxzfz624b95hb1ipjvd3mk177flmddcf24r";
+    };
+  });
+in
 
 stdenv.mkDerivation rec {
   name = "pspp-1.0.1";
@@ -12,16 +25,29 @@ stdenv.mkDerivation rec {
     sha256 = "1r8smr5057993h90nx0mdnff8nxw9x546zzh6qpy4h3xblp1la5s";
   };
 
+  # Adds the SAVE DATA COLLECTION command
+  patches = [
+    ./0001-New-command-SAVE-DATA-COLLECTION.patch
+    ./0002-tests-Add-missing-file.patch
+    ./0003-outfile-optional.patch
+    ./0004-mdd-more-info.patch
+  ];
+
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libxml2 readline zlib perl cairo gtk3 gsl
-    gtksourceview pango gettext
-    makeWrapper gsettings-desktop-schemas hicolor-icon-theme ];
+  buildInputs = [
+    libxml2 readline zlib perl cairo gsl gtksourceview pango gettext
+    automake_1_15 autoconf texinfo
+  ] ++ stdenv.lib.optionals (!headless) [
+    gtk3 makeWrapper gsettings-desktop-schemas hicolor-icon-theme
+  ];
 
   doCheck = false;
 
   enableParallelBuilding = true;
 
-  preFixup = ''
+  configureFlags = stdenv.lib.optional headless "--without-gui";
+
+  preFixup = stdenv.lib.optionalString (!headless) ''
     wrapProgram "$out/bin/psppire" \
      --prefix XDG_DATA_DIRS : "$out/share" \
      --prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS" \
